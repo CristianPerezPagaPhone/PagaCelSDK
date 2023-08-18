e5ca2d2d769687c0f61fdb06724fcdaf8eeec7d4
Cristian Perez <cristian.perez@pagaphone.com>
2023-08-18 10:36:07 -0600

    fist commit

---
+++ Pagacel/src/main/java/com/pagacel/sdk/compatibility/imeiCompatibility.kt
@@ -0,0 +1,38 @@
+package com.pagacel.sdk.compatibility
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.telephony.TelephonyManager
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class imeiCompatibility {
+    companion object {
+
+        private val REQUEST_READ_PHONE_STATE = 123
+
+        /*
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun getIMEI(activity: Activity, onIMEIReady: (String?) -> Unit) {
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_READ_PHONE_STATE)
+            } else {
+                val telephonyManager = activity.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                val imei = telephonyManager.imei
+                onIMEIReady(imei)
+            }
+        }*/
+
+
+        fun pruebasFunciones(context: Context): String? {
+            Toast.makeText(context,"Se integro funcion de forma correcta.", Toast.LENGTH_LONG).show()
+            return "Se integro funcion de forma correcta."
+
+        }
+    }
+}
