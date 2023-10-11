a51a9a6e2f0cfe6bd45272c328f1fba584520da6
Cristian Perez <cristian.perez@pagaphone.com>
2023-10-10 20:06:41 -0600

    ajustes de sdk

--- app/src/main/java/com/example/sdk/MainActivity.kt
+++ app/src/main/java/com/example/sdk/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.sdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.pagacel.sdk.compatibility.PagaCelCompatibility
+import com.example.pagacel.sdk.compatibility.orange_compatibility.classes.CallbackManager
+import com.example.pagacel.sdk.compatibility.orange_compatibility.models.responses.GetLineStatusModel
+import com.example.pagacel.sdk.compatibility.orange_compatibility.models.responses.GetLineStatusResponse
+import com.example.pagacel.sdk.compatibility.orange_compatibility.view.views.SettingsValidationView
+import com.example.sdk.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Ahora puedes acceder a tus vistas por su ID utilizando binding
+        val miTextView = binding.botonConfiguracion
+        val miTextView2 = binding.botonConfiguracion2
+
+
+        val callback = object : SettingsValidationView {
+
+            override fun showPlanInfo(lineResponse: GetLineStatusResponse) {
+                Toast.makeText(this@MainActivity, "Linea: ${lineResponse.model!!.mvnoInformation} con oferta:${lineResponse.model!!.currentOffert} y beneficios: ${lineResponse.model!!.currentOffert} vence el: ${lineResponse.model!!.currentOffert}",Toast.LENGTH_LONG).show()
+            }
+
+            override fun showPlanErrorInfo(message: String) {
+                Toast.makeText(this@MainActivity,message,Toast.LENGTH_LONG).show()
+            }
+
+            override fun showValidationOfLine() {
+                Toast.makeText(this@MainActivity, "Mostrar vista de OTP", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onTutorialBack() {
+                Toast.makeText(this@MainActivity, "Se cerro el tutorial del SDK", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        CallbackManager.setCallback(callback)
+
+        /*val intent = Intent(this, AutoSettingsMenuActivity::class.java)
+        startActivity(intent)*/
+
+        miTextView.setOnClickListener {
+            PagaCelCompatibility.showTutorialsMenu(this)
+        }
+
+        miTextView2.setOnClickListener{
+            PagaCelCompatibility.getLineStatus(this, "5586467852")
+        }
     }
+
 }
